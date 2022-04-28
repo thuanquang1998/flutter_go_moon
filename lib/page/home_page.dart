6fe679024432344f7bfae9db2bc161f79b97f4b9
thuanquang1998 <thuanquang2009@gmail.com>
2022-04-28 17:49:07 +0700

    first commit

---
+++ lib/page/home_page.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import '../widgets/custom_dropdown_button.dart';
+
+class HomePage extends StatelessWidget {
+  late double _deviceHeight, _deviceWidth;
+
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: _deviceWidth,
+          height: _deviceHeight,
+          padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
+          child: Stack(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _pageTitle(),
+                  _bookRideWidget(),
+                ],
+              ),
+              Align(
+                child: _astroImageWidget(),
+                alignment: Alignment.centerRight,
+              ),
+              Container(
+                child: Text("aaaaaaaa"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#GoMoon",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 70,
+        fontWeight: FontWeight.w800,
+      ),
+    );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      height: _deviceHeight * 0.50,
+      width: _deviceWidth * 0.65,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.contain,
+          image: AssetImage("assets/images/astro_moon.png"),
+        ),
+      ),
+    );
+  }
+
+  Widget _bookRideWidget() {
+    return Container(
+      height: _deviceHeight * 0.25,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _destinationDropdownWidget(),
+          _travellersInformationWidget(),
+          _rideButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget _destinationDropdownWidget() {
+    return CustomDropdownButtonClass(
+      values: [
+        'James Web Station',
+        'Prenere Station',
+      ],
+      width: _deviceWidth,
+    );
+  }
+
+  Widget _travellersInformationWidget() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CustomDropdownButtonClass(
+          values: const [
+            '1',
+            '2',
+            '3',
+            '4',
+          ],
+          width: _deviceWidth * 0.45,
+        ),
+        CustomDropdownButtonClass(
+          values: const [
+            'Economy',
+            'Business',
+            'First',
+            'Private',
+          ],
+          width: _deviceWidth * 0.40,
+        ),
+      ],
+    );
+  }
+
+  Widget _rideButton() {
+    return Container(
+      margin: EdgeInsets.only(bottom: _deviceHeight * 0.01),
+      width: _deviceWidth,
+      // color: Colors.white,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: MaterialButton(
+        onPressed: () {},
+        child: Text(
+          "Book Ride!",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
